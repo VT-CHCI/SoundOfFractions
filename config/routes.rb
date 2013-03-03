26838f61aca7765341c04cc997724839dd024e6d
Chris Frisina <github@specialorange.org>
2013-03-03 12:33:20 -0500

    fixing heroku

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,8 @@ SoundOfFractions::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'songs#index'
+  root :to => 'home#index'
+  # root :to => 'songs#index'
 
   # See how all your routes lay out with "rake routes"
 
