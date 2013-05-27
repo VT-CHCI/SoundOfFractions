2529b11d48c8d575a99deeb590f11f082530a0a8
Chris Frisina <github@specialorange.org>
2013-05-27 13:14:04 -0400

    refined

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ end
 gem 'sass-rails',   '~> 3.2.3'
 gem 'coffee-rails', '~> 3.2.1'
 
+gem 'colorize'
 gem 'jquery-ui-rails'
 gem 'jquery-rails'
 gem 'rails-backbone'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # User
-  puts 'SETTING UP DEFAULT USER LOGIN'
+  puts 'SETTING UP DEFAULT USER LOGIN'.colorize( :color => :light_blue, :background => :green )
   adminUser = User.create({ :email => "test@test.org", :password => "asdfasdf", :password_confirmation => "asdfasdf" }, :without_protection => true ).save!
   teacherUser1 = User.create({ :email => "tea@test.org", :password => "asdfasdf", :password_confirmation => "asdfasdf" }, :without_protection => true ).save!
   studentUser1 = User.create({ :email => "stu@test.org", :password => "asdfasdf", :password_confirmation => "asdfasdf" }, :without_protection => true ).save!
@@ -28,13 +28,13 @@
   p8 = Person.create({ firstName: "Teacher", lastName: "3", user_id: 8}, without_protection: true).save!
 
 # Roles
-  puts 'SETTING UP DEFAULT ROLES'
+  puts 'SETTING UP DEFAULT ROLES'.colorize( :color => :light_blue, :background => :green )
   role1 = Role.create({ :name => "Admin" }, :without_protection => true ).save!
   role2 = Role.create({ :name => "Teacher" }, :without_protection => true ).save!
   role3 = Role.create({ :name => "Student" }, :without_protection => true ).save!
 
 # UserRole
-  puts 'SETTING UP DEFAULT USER-ROLES'
+  puts 'SETTING UP DEFAULT USER-ROLES'.colorize( :color => :light_blue, :background => :green )
   ur1 = UserRole.create({ :user_id => 1, :role_id => role1 }, :without_protection => true ).save!
   ur2 = UserRole.create({ :user_id => 2, :role_id => 2 }, :without_protection => true ).save!
   ur3 = UserRole.create({ :user_id => 3, :role_id => 3 }, :without_protection => true ).save!
@@ -45,12 +45,12 @@
   ur8 = UserRole.create({ :user_id => 8, :role_id => 2 }, :without_protection => true ).save!
 
 # Class Instruction
-  puts 'SETTING UP DEFAULT CLASSES'
+  puts 'SETTING UP DEFAULT CLASSES'.colorize( :color => :light_blue, :background => :green )
   class1 = ClassInstruction.create({ :name => "math-101", :time => "Sun, 9 Dec 2012 12:00:00 -0500".to_datetime }, :without_protection => true ).save!
   class2 = ClassInstruction.create({ :name => "ss", :time => "Sun, 9 Dec 2012 12:00:00 -0500".to_datetime }, :without_protection => true ).save!
 
 # Songs
-  puts 'SETTING UP DEFAULTS SONGS'
+  puts 'SETTING UP DEFAULTS SONGS'.colorize( :color => :light_blue, :background => :green )
   song1 = Song.create({ :title => "Snare - 1", :content => '{"components":[{"label":"Snare","img":"snare.png","mute":false,"sample":"808_sd.m4a","measures":[{"beats":[{"selected":true},{"selected":true},{"selected":true},{"selected":true}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Hi Hat","img":"hihat.png","mute":true,"sample":"808_chh.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Kick Drum","img":"kick.png","mute":true,"sample":"808_bd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Synth","img":"synth.png","mute":true,"sample":"ambass.mp3","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"}]}', :user_id => 1 }, :without_protection => true ).save!
   song2 = Song.create({ :title => "Hi Hat - 1", :content => '{"components":[{"label":"Snare","img":"snare.png","mute":false,"sample":"808_sd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Hi Hat","img":"hihat.png","mute":true,"sample":"808_chh.m4a","measures":[{"beats":[{"selected":true},{"selected":true},{"selected":true},{"selected":true}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Kick Drum","img":"kick.png","mute":true,"sample":"808_bd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Synth","img":"synth.png","mute":true,"sample":"ambass.mp3","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"}]}', :user_id => 1 }, :without_protection => true ).save!
   song3 = Song.create({ :title => "Kick Drum - 1", :content => '{"components":[{"label":"Snare","img":"snare.png","mute":false,"sample":"808_sd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Hi Hat","img":"hihat.png","mute":true,"sample":"808_chh.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Kick Drum","img":"kick.png","mute":true,"sample":"808_bd.m4a","measures":[{"beats":[{"selected":true},{"selected":true},{"selected":true},{"selected":true}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Synth","img":"synth.png","mute":true,"sample":"ambass.mp3","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"}]}', :user_id => 1 }, :without_protection => true ).save!
@@ -61,12 +61,12 @@
   song8 = Song.create({ :title => "Synth - 2", :content => '{"components":[{"label":"Snare","img":"snare.png","mute":false,"sample":"808_sd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Hi Hat","img":"hihat.png","mute":true,"sample":"808_chh.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Kick Drum","img":"kick.png","mute":true,"sample":"808_bd.m4a","measures":[{"beats":[{"selected":false},{"selected":false},{"selected":false},{"selected":false}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"},{"label":"Synth","img":"synth.png","mute":true,"sample":"ambass.mp3","measures":[{"beats":[{"selected":true},{"selected":true},{"selected":true},{"selected":true}],"label":"0/4","numberOfBeats":0,"divisions":8}],"active":true,"signature":4,"representation":"fraction"}]}', :user_id => 2 }, :without_protection => true ).save
 
 # Assignments
-  puts 'SETTING UP DEFAULT ASSIGNMENTS'
+  puts 'SETTING UP DEFAULT ASSIGNMENTS'.colorize( :color => :light_blue, :background => :green )
   assignment1 = Assignment.create({ :name => 'Assignment 1', :fractionRepresentations => 'fraction', :measureRepresentations => 'circular-pie', :visibleWindows => '', :visibleWindowItems => '', :tempoEditable => true, :lowerRangeOfTempo => 30, :upperRangeOfTempo => 400, :lowerRangeOfBeatsPerMeasure => 2, :upperRangeOfBeatsPerMeasure => 14, :looping => false }, :without_protection => true ).save!
   assignment2 = Assignment.create({ :name => 'Assignment 1', :fractionRepresentations => 'fraction,percent', :measureRepresentations => 'circular-bead,circular-pie', :visibleWindows => '', :visibleWindowItems => '', :tempoEditable => true, :lowerRangeOfTempo => 30, :upperRangeOfTempo => 400, :lowerRangeOfBeatsPerMeasure => 2, :upperRangeOfBeatsPerMeasure => 14, :looping => true }, :without_protection => true ).save!
 
 # Settings
-  puts 'SETTING UP DEFAULT SETTINGS'
+  puts 'SETTING UP DEFAULT SETTINGS'.colorize( :color => :light_blue, :background => :green )
   settings1 = Setting.create({ user_id: 1, mic_level: 1 }, :without_protection => true).save!
   settings2 = Setting.create({ user_id: 2, mic_level: 1 }, :without_protection => true).save!
   settings3 = Setting.create({ user_id: 3, mic_level: 1 }, :without_protection => true).save!
@@ -77,13 +77,13 @@
 
 
 # ClassInstruction Assignment
-  puts 'SETTING UP DEFAULT Class-Assignments'
+  puts 'SETTING UP DEFAULT Class-Assignments'.colorize( :color => :light_blue, :background => :green )
   cia1 = ClassAssignment.create({ class_instruction_id: 1, assignment_id:1 }, :without_protection => true).save! 
   cia2 = ClassAssignment.create({ class_instruction_id: 1, assignment_id:2 }, :without_protection => true).save! 
   cia3 = ClassAssignment.create({ class_instruction_id: 2, assignment_id:1 }, :without_protection => true).save! 
 
 # Participants
-  puts 'SETTING UP DEFAULT Participants'
+  puts 'SETTING UP DEFAULT Participants'.colorize( :color => :light_blue, :background => :green )
   # Admin ATTENDS class 1 and 2
   p1a = Participant.create({ person_id: 1, class_instruction_id: 1 }, :without_protection => true).save!
   p1b = Participant.create({ person_id: 1, class_instruction_id: 2 }, :without_protection => true).save!
