a0fdfeaa4f6c67a43c8d4fe6ed6076b80e278f4b
special <github@specialorange.org>
2013-06-19 15:38:22 -0400

    secret token now tracking

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundOfFractions::Application.config.secret_token = '0f77a6e7cb6b15903a82d12bed2314deb95710f23c6511a06fbe2883cc9238d6521a29dd44ae5ed21da12af6b0627083d443daf3ae0c7aa09f2ab185cfe7de7a'
+SoundOfFractions::Application.config.secret_key_base = 'xx0f77a6e7cb6b15903a82d12bed2314deb95710f23c6511a06fbe2883cc9238d6521a29dd44ae5ed21da12af6b0627083d443daf3ae0c7aa09f2ab185cfe7de7a'
