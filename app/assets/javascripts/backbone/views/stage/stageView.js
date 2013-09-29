7c5e23efc54cd14af74504f5caabe9854ae47961
special <github@specialorange.org>
2013-09-29 18:16:24 -0400

    setup for pres

--- app/assets/javascripts/backbone/views/stage/stageView.js
+++ app/assets/javascripts/backbone/views/stage/stageView.js
@@ -55,14 +55,14 @@ define([
       this.manuallyCreatedMeasureRepresentationCollection = new RepresentationsCollection;
       this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
       // add a bead rep
-      this.manuallyCreatedRepresentationModel = new RepresentationModel({representationType:'bar'});
-      this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
-      // add a line rep
       this.manuallyCreatedRepresentationModel = new RepresentationModel({representationType:'bead'});
       this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
       // add a line rep
-      this.manuallyCreatedRepresentationModel = new RepresentationModel({representationType:'line'});
-      this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
+      // this.manuallyCreatedRepresentationModel = new RepresentationModel({representationType:'bead'});
+      // this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
+      // add a line rep
+      // this.manuallyCreatedRepresentationModel = new RepresentationModel({representationType:'line'});
+      // this.manuallyCreatedMeasureRepresentationCollection.add(this.manuallyCreatedRepresentationModel);
 
       this.manuallyCreatedMeasuresCollection = new MeasuresCollection;
       this.manuallyCreatedMeasuresCollection.add({
