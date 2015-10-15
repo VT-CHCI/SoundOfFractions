bfb998969fddc5bdcfcf4035818c257054a3ecaa
special <github@specialorange.org>
2015-10-15 00:04:27 -0400

    https server working with nconf and environment variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var https = require('https');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var simpleApp = express();
 var RSVP = require('rsvp');
 var CryptoJS = require('crypto-js');
 var uuid = require('node-uuid');
@@ -10,18 +10,65 @@ var winston = require('winston');
 var nconf = require('nconf');
 var fs = require('fs');
 
+console.log('process.cwd()');
+console.log(process.cwd());
+
+console.log('before');
+console.log(__dirname);
+
+console.log('process.env.SOF_CONFIG');
+console.log(process.env.SOF_CONFIG);
+
 // First consider command line arguments and environment variables, respectively.
 // So it would be started with something like node server.js --foo bar
 //  and the parameter foo will have a value of bar
 nconf
+  // To access a value in the file, use nconf.get('key') to return a value
+  // .file({ file: '/home/collin/SoundOfFractions/config.json' })
   .argv()
   .env()
-  // To access a value in the file, use nconf.get('key') to return a value
-  .file({ file: '/home/collin/SoundOfFractions/config.json' });
+  .file({ file: nconf.get('CONFIG') });
+
+console.log('CONFIG');
+console.log(nconf.get('CONFIG'));
+
+console.log('after');
+console.log(__dirname);
+console.log(nconf.get('key'));
+console.log(nconf.get('cert'));
+console.log(nconf.get('httpsPort'));
+console.log(nconf.get('sqlDatabase'));
+console.log(nconf.get('sqlUsername'));
+console.log(nconf.get('sqlPassword'));
 
 app.use(express.static(__dirname + '/app'));
-
 app.use(cookieParser());
+
+console.log('after-after');
+console.log(__dirname);
+
+simpleApp.listen(8080);
+simpleApp.get('*',function(req,res){  
+  console.log('gh');
+  res.redirect(nconf.get('baseURL')+req.url)
+})
+
+var https = require('https');
+var secureServer = https.createServer({
+    key: fs.readFileSync(nconf.get('key'), 'utf8'),
+    cert: fs.readFileSync(nconf.get('cert'), 'utf8'),
+    ca: [
+            fs.readFileSync(nconf.get('ca1'), 'utf8'),
+            fs.readFileSync(nconf.get('ca2'), 'utf8'),
+            fs.readFileSync(nconf.get('ca3'), 'utf8')
+        ],
+    rejectUnauthorized: false
+}, app).listen(nconf.get('httpsPort'), function() {
+    // var host = secureServer.address().address;
+    var port = secureServer.address().port;
+    start();
+    console.log('Secure Express server listening at localhost:%s', port);
+});
 
 var Sequelize = require('sequelize'),
                           // ('database', 'username', 'password');
@@ -293,16 +340,6 @@ function start() {
   });
 }
 
-var secureServer = https.createServer({
-    key: fs.readFileSync(nconf.get('key'), 'utf8'),
-    cert: fs.readFileSync(nconf.get('cert'), 'utf8'),
-    rejectUnauthorized: false
-}, app).listen(nconf.get('httpsPort'), function() {
-    var host = secureServer.address().address;
-    var port = secureServer.address().port;
-    console.log('Secure Express server listening at localhost:%s', port);
-});
-
 // var server = app.listen(3000, function() {
 //     var host = server.address().address;
 //     var port = server.address().port;
@@ -310,6 +347,13 @@ var secureServer = https.createServer({
 //   console.log('App listening at localhost:%s', port);
 //   start();
 // });
+
+
+app.get('/hello', function (req, res) {
+  var msg = 'hellooooo';
+  console.log(msg);
+  res.send(msg);
+});
 
 app.post('/api/login', function (req, res) {
   console.log('Getting a login request with a body of: ');
